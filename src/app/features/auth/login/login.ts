9d35f9672ca897a2b8b60baacd32ebb3fe830cc1
Riandika <riandikatkj@gmail.com>
2026-09-12 01:08:37 +0700

    feat: add login form validation

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,16 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  imports: [RouterLink],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
 export class Login {
+  private readonly fb = inject(FormBuilder);
+
   showPassword = signal(false);
+
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
   togglePassword(): void {
     this.showPassword.update((value) => !value);
+  }
+
+  isInvalid(controlName: 'email' | 'password'): boolean {
+    const control = this.loginForm.controls[controlName];
+
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm.getRawValue());
   }
 }
