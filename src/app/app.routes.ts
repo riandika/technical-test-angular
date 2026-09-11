c84f5eb27d762f4a8058f4367892cb5affa4ea9a
Riandika <riandikatkj@gmail.com>
2026-09-11 22:41:17 +0700

    feat: setup application structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadComponent: () =>
+            import(
+                './features/home/home/home'
+            ).then((m) => m.Home)
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import(
+                './features/auth/login/login'
+            ).then((m) => m.Login)
+    },
+    {
+        path: 'products',
+        loadComponent: () =>
+            import(
+                './features/products/product-list/product-list'
+            ).then((m) => m.ProductList)
+    },
+    {
+        path: 'products/:id',
+        loadComponent: () =>
+            import(
+                './features/products/product-detail/product-detail'
+            ).then((m) => m.ProductDetail),
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    },
+];
