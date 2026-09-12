f2e0f06fed468f1d83acb906a79d7c020b08d068
Riandika <riandikatkj@gmail.com>
2026-09-13 01:30:48 +0700

    feat: add footer

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-footer',
   styleUrl: './footer.css',
   templateUrl: './footer.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Footer {
   readonly currentYear = new Date().getFullYear();
